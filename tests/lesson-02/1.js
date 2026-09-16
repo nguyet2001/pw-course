9d0598db3a03e8269b17129ed8140e9974e39613
Anh Nguyet <luyenanhnguyet.ptit@gmail.com>
2026-09-16 12:13:45 +0700

    bt buoi 2

---
+++ tests/lesson-02/1.js
@@ -0,0 +1,17 @@
+// Cach 1
+
+let a = 0 ;
+let b = 1 ;
+
+console.log(a%2);
+
+console.log(b%2);
+
+//Cach 2
+  let a = 5;
+  if (a%2==0){
+    console.log (a%2);
+  }
+  else {
+    console.log(a%2);
+  }
